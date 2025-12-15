9fc5d61f3781482f287655abbaf724d0ba4ce766
tharun2007s-tech <tharun2007s@gmail.com>
2025-12-15 17:50:14 +0530

    Success

---
+++ D_Flipflop.v
@@ -0,0 +1,11 @@
+module D_Flipflop (
+    input  wire clk, rst, D,
+    output reg  Q
+);
+    always @(posedge clk or posedge rst) begin
+        if (rst)
+            Q <= 1'b0;
+        else
+            Q <= D;      
+    end
+endmodule
